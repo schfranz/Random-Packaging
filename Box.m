f4d91497866083a5ed6d3d1094f774bcfb5d9869
Franz Wurst <tufu@gmx.de>
2018-07-24 18:51:01 -0700

    added Box class to package

---
+++ Box.m
@@ -0,0 +1,170 @@
+%TODO:
+% - update
+% - write checks for nonnegative inputs (also Sphere and Cylinder)
+
+%% This class defines a 3-D rectangle with the longest side as its height 
+    %(aligned with z), the medium side as its width (aligned with y), and 
+    %the shortest side as its depth (aligned with x).
+%%
+classdef Box < ShapeInterface
+    %inherited properties:
+        %volume     %volume of box       %protected
+        %shape      %box                 %protected
+        %height     %longest side
+        %width      %medium side
+        %depth      %shortest side
+        %center     %center coordinates     %default: [0,0,0]
+        
+    properties (SetObservable)
+        diagonal  %body diagonal
+    end
+    
+    methods
+        %construtor method
+        function obj = Box(height, width, depth, center)
+            obj.shape = 'box';
+            addlistener(obj, 'height', 'PostSet', @Box.updateProps);
+            addlistener(obj, 'width', 'PostSet', @Box.updateProps);
+            addlistener(obj, 'depth', 'PostSet', @Box.updateProps);
+            %addlistener(obj, 'center', 'PostSet', @Box.updateLocation);
+            switch nargin
+                case 0 %no argument constructor
+                otherwise %set basic properties of box
+                    obj.height = height;
+                    obj.width = width;
+                    obj.depth = depth;
+                    obj.volume = obj.height * obj.width * obj.depth;
+                    %check this
+                    obj.diagonal = 2*pdist([obj.center; (obj.center - ...
+                        [obj.depth/2, obj.width/2, obj.height/2])]);
+            end
+            if (nargin >= 4) %box centered around provided origin
+                obj.center = center;
+            end
+        end
+    end
+        
+    methods %other methods required to implement interface
+        %function obj = getSurfaceCoords(obj, nMeshPoints) %nMeshPoints is number of points in each direction
+        %    
+        %end
+        
+        %returns true if a point falls within the specified shape
+        %   (including surface)
+        function bool = isInShape(obj, coords)
+            if (length(coords) ~= 3)
+                error(obj.errMessBadCoord)
+            end
+            
+            %{
+            a = obj.center(1) + height/2;
+            b = obj.center(1) - height/2;
+            c = obj.center(2) + width/2;
+            d = obj.center(2) - width/2;
+            e = obj.center(3) + depth/2;
+            f = obj.center(3) - depth/2;
+            %}
+            
+            %decide if point is in shape or not
+            %{
+            if ((coords(1) < obj.center(1) + obj.height/2) && ...
+                    (coords(1) > obj.center(1) - obj.height/2) && ...
+                    (coords(2) < obj.center(2) + obj.width/2) && ...
+                    (coords(2) > obj.center(2) - obj.width/2) && ...
+                    (coords(3) < obj.center(3) + obj.depth/2) && ...
+                    (coords(3) > obj.center(3) - obj.depth/2))
+                bool = true;
+            else
+                bool = false;
+            end
+            %}
+            
+            %does this work?
+            %%{
+            if (isOnLine(coords(1), obj.center(1), obj.depth) && ...
+                    isOnLine(coords(2), obj.center(2), obj.width) && ...
+                    isOnLine(coords(3), obj.center(3), obj.height))
+                bool = true;
+            else
+                bool = false;
+            end
+            %%}
+        end
+        
+        %returns true if a point is on the surface of the specified shape
+        function bool = isOnShapeSurf(obj, coords)
+            if (length(coords) ~= 3)
+                error(obj.errMessBadCoord)
+            end
+            
+            %does this work?
+            %decide if point is on shape surface or not
+            if (((coords(3) == (obj.center(3) - obj.height/2)) && ... %on the xy plane?
+                    isOnLine(coords(1), obj.center(1), obj.depth) && ...
+                    isOnLine(coords(2), obj.center(2), obj.width)) || ...
+                    ((coords(1) == (obj.center(1) - obj.depth/2)) && ... %on the yz plane?
+                    isOnLine(coords(2), obj.center(2), obj.width) && ...
+                    isOnLine(coords(3), obj.center(3), obj.height)) || ...
+                    ((coords(2) == (obj.center(2) - obj.width/2)) && ... %on the zx plane?
+                    isOnLine(coords(3), obj.center(3), obj.height) && ...
+                    isOnLine(coords(1), obj.center(1), obj.depth)))
+                bool = true;
+            else
+                bool = false;
+            end
+        end
+        
+        %moves object in xyz as specified by vector coordChange
+        function obj = move(obj, coordChange)
+            obj.center = obj.center + coordChange;
+        end
+        
+        function obj = rotate(obj, ~, ~, ~, ~)%angle, x, y, z)
+            fprintf('Hehehe, cannot rotate boxes yet...\n')
+        end
+    end
+        
+    methods (Static)
+        %update function that respond to data update by user
+        function updateProps(~, eventData) %first argument is class info?
+            obj = eventData.AffectedObject;
+            switch eventData.Source.Name
+                case 'height'
+                    obj.volume = obj.height * obj.width * obj.depth;
+                    obj.diagonal = 2*pdist([obj.center; (obj.center - ...
+                        [obj.depth/2, obj.width/2, obj.height/2])]);
+                case 'width'
+                    obj.volume = obj.height * obj.width * obj.depth;
+                    obj.diagonal = 2*pdist([obj.center; (obj.center - ...
+                        [obj.depth/2, obj.width/2, obj.height/2])]);
+                case 'depth'
+                    obj.volume = obj.height * obj.width * obj.depth;
+                    obj.diagonal = 2*pdist([obj.center; (obj.center - ...
+                        [obj.depth/2, obj.width/2, obj.height/2])]);
+            end
+        end
+        
+        %update function that responds to location update by user
+        %{
+        function updateLocation(~, eventData)
+            obj = eventData.AffectedObject;
+            switch eventData.Source.Name
+                case 'center'
+                    %updateShapeMesh
+            end
+        end
+        %}
+    end
+    
+    methods (Private) %private functions for exclusive use of this class
+        %function that determines if a given number is on a line specified
+        %in terms of the center of the line and its length (1-D!)
+        function bool = isOnLine(number, center, length)
+            if (number <= (center - length/2) && number >= center - (length/2))
+                bool = true;
+            else
+                bool = false;
+            end            
+        end        
+    end
+end
