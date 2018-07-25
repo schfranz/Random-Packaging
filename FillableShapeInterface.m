60b6f39811de763e573ddff6a1fc3df72cbe0653
Franz Wurst <tufu@gmx.de>
2018-07-25 15:41:46 -0700

    added class variables from CanisterInterface

--- FillableShapeInterface.m
+++ FillableShapeInterface.m
@@ -4,17 +4,38 @@ classdef (Abstract) FillableShapeInterface < ShapeInterface
     %inherited properties:
         %volume     %volume of shape        %protected
         %shape      %shape type             %protected
-        %width      %width of shape
         %height     %height of shape
+        %width      %width of shape
+        %depth      %depth of shape
         %center     %center coordinates     %default: [0,0,0]
     
     %%
     %%VARIABLES
+    %%PUBLIC%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    properties
+        nFillShapesExp = 10; %number of fill shapes user expects to place
+    end
+    
     %%PROTECTED%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     properties (SetAccess = protected)
-        freeVolume          %free volume that remains when filling shape is inside
-        isFilled = false    %boolean that is true if a shape is inside the outer shape
+        allowedFillShapes = 'sphere';   %FillShapes that can be used (remove default later)
+        mixedShapes = false;        %boolean that is true if FillShapes are different (sizes or shapes)
+        nFillShapes = 0;            %number of FillShapes present
+        freeVolume                  %free volume that remains when FillShapes are inside
+        isFilled = false            %boolean that is true if a shape is inside the outer shape
+        shapeLocs                   %list of FillShape IDs, actual center coordinates, and shape types: [ID, x, y, z, shape]
+        shapeLocsOrig               %same as shapeLocs, but with originally intended center coordinates
+        coordListX                  %list of actual x coordinates, shape IDs, and types: [x, ID, shape]
+        coordListY                  %list of actual y coordinates, shape IDs, and types: [y, ID, shape]
+        coordListZ                  %list of actual z coordinates, shape IDs, and types: [z, ID, shape]
     end
+    
+    properties (Hidden, GetAccess = protected)
+        errMessInvalidShape = 'This fill shape type is not supported \n';
+        errMessLargeFillShape = 'The fill shape provided is too large for the canister';
+    end
+    
+    %%PRIVATE%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     %%
     %%FUNCTIONS
