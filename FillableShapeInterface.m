63b6ac539cd74908a598941ad97809c5bcdadaa8
Franz Wurst <tufu@gmx.de>
2018-07-25 15:56:04 -0700

    added class functions from canisterInteface

--- FillableShapeInterface.m
+++ FillableShapeInterface.m
@@ -20,7 +20,7 @@ classdef (Abstract) FillableShapeInterface < ShapeInterface
     properties (SetAccess = protected)
         allowedFillShapes = 'sphere';   %FillShapes that can be used (remove default later)
         mixedShapes = false;        %boolean that is true if FillShapes are different (sizes or shapes)
-        nFillShapes = 0;            %number of FillShapes present
+        nFillShapes = 0;            %actual number of FillShapes present
         freeVolume                  %free volume that remains when FillShapes are inside
         isFilled = false            %boolean that is true if a shape is inside the outer shape
         shapeLocs                   %list of FillShape IDs, actual center coordinates, and shape types: [ID, x, y, z, shape]
@@ -33,6 +33,10 @@ classdef (Abstract) FillableShapeInterface < ShapeInterface
     properties (Hidden, GetAccess = protected)
         errMessInvalidShape = 'This fill shape type is not supported \n';
         errMessLargeFillShape = 'The fill shape provided is too large for the canister';
+        warnMessNshapesIncr = ['Increasing the number of FillShapes present ' ...
+            'beyond the specified number of expected FillShapes (nFillShapesExp) ' ...
+            'may slow down placement of additional shapes. Consider increasing ' ...
+            'nFillShapesExp.']; %figure out newline
     end
     
     %%PRIVATE%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -41,6 +45,29 @@ classdef (Abstract) FillableShapeInterface < ShapeInterface
     %%FUNCTIONS
     %%ABSTRACT%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     methods (Abstract)
-        canFillHere(obj)
-    end    
+        addShape(obj)       %function that adds a fill shape 
+        deleteShape(obj)    %function that deletes a fill shape
+        %canFillHere(obj) %?
+        %dropEvenly(obj)     %function to evenly distribute fill shapes (not important rn)
+        %dropRandomly(obj)   %function to randomly distribute fill shapes
+        %getSurfaceCoords(obj) %this should already exist as part of ShapeInterface
+    end
+    
+    %%SET&GET%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %%PROTECTED%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    methods (Access = protected, Hidden)
+        %function that checks if a suggested FillShape is allowed
+        function bool = checkValidFillShape(obj, fillShape)
+            bool = false;
+            if (obj.allowedFillShapes(:) ~= fillShape.shape)
+                error(obj.errMessInvalidShape)
+            else
+                bool = true;
+            end
+        end
+        %findDropArea(obj)  %function that determines where shapes can be dropped
+        %moveObj(obj)       %function that moves an object if it overlaps with sth
+        %rotateObj(obj)     %function that rotates an object if it overlaps
+    end
 end
