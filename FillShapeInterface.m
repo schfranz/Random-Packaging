90e626d82a20f4acfa271fe6c138092b0b13f241
Franz Wurst <tufu@gmx.de>
2018-07-25 12:20:49 -0700

    created new interface FillShapeInterface

---
+++ FillShapeInterface.m
@@ -0,0 +1,22 @@
+classdef (Abstract) FillShapeInterface < ShapeInterface
+    %abstract class that describes a 3-D shape that can fill another 3-D shape
+    %inherited properties:
+        %volume     %volume of shape        %protected
+        %shape      %shape type             %protected
+        %height     %height of shape
+        %width      %width of shape
+        %depth      %depth of shape
+        %center     %center coordinates     %default: [0,0,0]
+    
+    %%
+    %%VARIABLES
+    %%PROTECTED%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %%
+    %%FUNCTIONS
+    %%ABSTRACT%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   
+    
+    
+    
+    
+end
