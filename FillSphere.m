846053f54869f59920954c206741ff9497561f4e
Franz Wurst <tufu@gmx.de>
2018-07-25 12:37:38 -0700

    created class FillSphere

---
+++ FillSphere.m
@@ -0,0 +1,32 @@
+classdef FillSphere < Sphere & FillShapeInterface %order determines which superclass constructor is used!
+    %class that inherits from Sphere and implements FillShapeInterface
+    %FillSphere is a Sphere object that can be used to fill a shape 
+    %   implementing FillableShapeInterface 
+    
+    %inherited properties:
+        %from class Sphere:
+        %volume     %volume of sphere       %protected
+        %shape      %sphere                 %protected
+        %height     %diameter
+        %width      %diameter
+        %depth      %diameter
+        %center     %center coordinates     %default: [0,0,0]
+        %radius     %radius of sphere
+        %diameter   %diameter of sphere
+        %from abstract class FillShapeInterface:
+        
+        
+    %%
+    %%VARIABLES
+    %%PROTECTED%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %%
+    %%FUNCTIONS
+    %%CONSTRUCTOR%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    
+    
+    
+    
+    
+end
